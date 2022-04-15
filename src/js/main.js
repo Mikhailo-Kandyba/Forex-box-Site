da53ae07dd74b518bf9167e618638a79c6e0b682
Mikhailo <mikhailo.kandyba0@gmail.com>
2022-04-15 15:49:10 +0300

    description-service block

--- src/js/main.js
+++ src/js/main.js
@@ -77,3 +77,10 @@ $(".btn-menu").on('click', function(e) {
     });
 })(jQuery);
 //end
+
+
+// When the user clicks on div, open the popup
+function myFunction() {
+    var popup = document.getElementById("myPopup");
+    popup.classList.toggle("show");
+}
