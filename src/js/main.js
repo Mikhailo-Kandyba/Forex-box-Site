6951ce2ca49bded5c4228be8c0d02ed846fdb58b
Mikhailo <mikhailo.kandyba0@gmail.com>
2022-04-15 11:15:16 +0300

    desctop version

---
+++ src/js/main.js
@@ -0,0 +1,7 @@
+//btn-menu
+$(".btn-menu").on('click', function(e) {
+    e.preventDefault();
+    $(this).toggleClass("active");
+    $(".menu-navigation-wrap").toggleClass('show-menu');
+    $("body").toggleClass('overflow-hidden');
+});
