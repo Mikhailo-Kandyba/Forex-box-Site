05f73fe9e168eb0870f92ad7110e0777cfccfa36
Mikhailo <mikhailo.kandyba0@gmail.com>
2022-04-23 00:29:03 +0300

    Marketplace

--- src/js/main.js
+++ src/js/main.js
@@ -1242,3 +1242,37 @@ $(function(){
 });
 
 
+$('.card-product-slide').slick({
+    dots: true,
+    prevArrow: $('.prev'),
+    nextArrow: $('.next'),
+    infinite: true,
+    speed: 300,
+    slidesToShow: 4,
+    slidesToScroll: 1,
+    responsive: [
+        {
+            breakpoint: 1024,
+            settings: {
+                slidesToShow: 1,
+                slidesToScroll: 1,
+                infinite: true,
+                dots: true
+            }
+        },
+        {
+            breakpoint: 767,
+            settings: {
+                slidesToShow: 1,
+                slidesToScroll: 1
+            }
+        },
+        {
+            breakpoint: 480,
+            settings: {
+                slidesToShow: 1,
+                slidesToScroll: 1
+            }
+        }
+    ]
+});
